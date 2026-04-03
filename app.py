51b620d66ca8f049ff2b50154dde8a1e0a0b22ef
Bushsmith3300 <bhbush5@gmail.com>
2026-04-03 01:26:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, make_response
-from questions_data import QUESTIONS  # <-- this is the only import you need
+from chem_questions2 import questions2  # <-- this is the only import you need
 
 app = Flask(__name__)
 
 @app.route('/questions')
 def get_questions():
-    response = make_response(jsonify(QUESTIONS))
+    response = make_response(jsonify(questions2))
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate'
     response.headers = 'no-cache'
     response.headers = '0'
